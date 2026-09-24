1cb7105a98a2485296c7a3565d2d8a79e49613ce
markup54 <marzenakupka@tlen.pl>
2026-09-24 08:55:34 +0200

    widok aplikacji

--- app/src/main/java/pl/zabrze/zs10/retrofit_gr2_4p/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/retrofit_gr2_4p/MainActivity.java
@@ -3,12 +3,28 @@ package pl.zabrze.zs10.retrofit_gr2_4p;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    RadioGroup radioGroupPytania;
+    RadioButton radioButtonA, radioButtonB, radioButtonC;
+    Button buttonNastepne;
+    TextView textViewTrescPytania;
+    ArrayList<Pytanie> listaPytan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        radioButtonA = findViewById(R.id.radioButton);
+        radioButtonB = findViewById(R.id.radioButton2);
+        radioButtonC = findViewById(R.id.radioButton3);
+        radioGroupPytania = findViewById(R.id.radioGroup);
+        buttonNastepne = findViewById(R.id.button);
+        textViewTrescPytania = findViewById(R.id.textViewPytanie);
     }
 }
